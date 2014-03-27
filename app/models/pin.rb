bd11b8edee6b73010c623ad2c4ce5dee0684bb62
Thomas Hoag <thmhoag@gmail.com>
2014-03-26 23:47:12 -0400

    Added names to users, added validation for pins/users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,7 @@ class Pin < ActiveRecord::Base
 		has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
 		do_not_validate_attachment_file_type :image
+
+		validates :description, presence: true
+		validates :image, presence: true
 end
